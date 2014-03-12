46431b242f688923d944f927e2b64a53fb09a782
schreon <schreon.loeder@gmail.com>
2014-03-12 13:47:06 +0100

    trying to get location test running

--- app/src/test/java/morepeople/android/app/LocationWrapperTest.java
+++ app/src/test/java/morepeople/android/app/LocationWrapperTest.java
@@ -1,8 +1,10 @@
 package morepeople.android.app;
 
 import android.content.Context;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -15,6 +17,7 @@ import org.robolectric.shadows.ShadowContext;
 import org.robolectric.shadows.ShadowLocationManager;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -66,7 +69,7 @@ public class LocationWrapperTest {
         ShadowLocationManager slm = shadowOf(instanceOfLocationManager);
         slm.setProviderEnabled(LocationManager.NETWORK_PROVIDER, true);
 
-        Location fakeLocation = new Location(LocationManager.NETWORK_PROVIDER);
+        final Location fakeLocation = new Location(LocationManager.NETWORK_PROVIDER);
         fakeLocation.setLongitude(123);
         fakeLocation.setLatitude(456);
 
@@ -76,12 +79,14 @@ public class LocationWrapperTest {
             @Override
             public void gotInstantTemporaryLocation(Location location) {
                 assertNotNull(location);
+                assertEquals(fakeLocation, location);
                 checkMap.put("gotInstantTemporaryLocation", true);
             }
 
             @Override
             public void gotFallbackLocation(Location location) {
                 assertNotNull(location);
+                assertEquals(fakeLocation, location);
                 // should not end up here
                 checkMap.put("gotFallbackLocation", true);
             }
@@ -97,10 +102,72 @@ public class LocationWrapperTest {
 
         assertTrue(checkMap.get("gotInstantTemporaryLocation"));
 
-        //sleep(50); // this is very ugly and should be refactored
         Robolectric.runUiThreadTasksIncludingDelayedTasks(); // wait for all async tasks to finish
         Robolectric.runBackgroundTasks();
 
         assertTrue(checkMap.get("gotFallbackLocation"));
+    }
+
+    @Test
+    public void shouldProvideNewLocation() throws InterruptedException {
+        final Map<String, Boolean> checkMap = new HashMap<String, Boolean>();
+
+        Context context = activity.getBaseContext();
+
+        LocationManager instanceOfLocationManager = (LocationManager) Robolectric.application.getSystemService(Context.LOCATION_SERVICE);
+        ShadowLocationManager slm = shadowOf(instanceOfLocationManager);
+        slm.setProviderEnabled(LocationManager.NETWORK_PROVIDER, true);
+
+        final Location fakeLocation = new Location(LocationManager.NETWORK_PROVIDER);
+        fakeLocation.setLongitude(123);
+        fakeLocation.setLatitude(456);
+        slm.setLastKnownLocation(LocationManager.NETWORK_PROVIDER, fakeLocation);
+
+
+        final Location changedLocation = new Location(LocationManager.NETWORK_PROVIDER);
+        changedLocation.setLongitude(666);
+        changedLocation.setLatitude(333);
+//        changedLocation.setBearing(0);
+//        changedLocation.setSpeed(0);
+//        changedLocation.setAltitude(0);
+//        changedLocation.setTime(new Date().getTime());
+//        changedLocation.setProvider(LocationManager.NETWORK_PROVIDER);
+//        changedLocation.setAccuracy(1);
+
+        LocationResponseHandler locationResponseHandler = new LocationResponseHandler(){
+            @Override
+            public void gotInstantTemporaryLocation(Location location) {
+                assertNotNull(location);
+                assertEquals(fakeLocation, location);
+                checkMap.put("gotInstantTemporaryLocation", true);
+            }
+
+            @Override
+            public void gotFallbackLocation(Location location) {
+                // should not get here!
+                fail();
+            }
+
+            @Override
+            public void gotNewLocation(Location location) {
+                assertNotNull(location);
+                assertEquals(changedLocation, location);
+                checkMap.put("gotNewLocation", true);
+            }
+        };
+
+        locationWrapper.requestLocation(activity.getBaseContext(),locationResponseHandler, 60000);
+
+        assertTrue(checkMap.get("gotInstantTemporaryLocation"));
+
+        Robolectric.runUiThreadTasksIncludingDelayedTasks(); // wait for all async tasks to finish
+        Robolectric.runBackgroundTasks();
+
+        slm.simulateLocation(changedLocation);
+
+        Robolectric.runUiThreadTasksIncludingDelayedTasks(); // wait for all async tasks to finish
+        Robolectric.runBackgroundTasks();
+
+        assertTrue(checkMap.get("gotNewLocation"));
     }
 }
